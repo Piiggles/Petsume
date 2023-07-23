74489641cbc98f2fc9579e010af3394b900acc43
Dyllan <dphamada92@gmail.com>
2023-07-23 01:35:38 -0700

    created onclick function that will send it to the postCreate html

--- js/script.js
+++ js/script.js
@@ -5,24 +5,28 @@ const imgArray = [
       "https://img.freepik.com/premium-photo/golden-retriever-lying-panting-isolated-white_191971-16974.jpg",
     title: "dog",
     description: "Dog hanging out",
+    text: "There is a dog!",
   },
   {
     image:
       "https://img.freepik.com/premium-photo/golden-retriever-lying-panting-isolated-white_191971-16974.jpg",
     title: "dog",
     description: "Dog hanging out",
+    text: "There is a dog!",
   },
   {
     image:
       "https://img.freepik.com/premium-photo/golden-retriever-lying-panting-isolated-white_191971-16974.jpg",
     title: "dog",
     description: "Dog hanging out",
+    text: "There is a dog!",
   },
   {
     image:
       "https://img.freepik.com/premium-photo/golden-retriever-lying-panting-isolated-white_191971-16974.jpg",
     title: "dog",
     description: "Dog hanging out",
+    text: "There is a dog!",
   },
 ];
 
@@ -33,8 +37,13 @@ imgArray.map((element) => {
         <ul>
             <li class="img-card card">
                 <img src="${element.image}" alt="${element.description}" class="img-src">
+            <p class="image-text">${element.text}</p>
             </li>
         </ul>
     </div>`;
   document.getElementById("all-post").appendChild(cardDiv);
 });
+
+document.getElementById("create-post").onclick = function() {
+  location.href = "./postCreate.html"
+};
